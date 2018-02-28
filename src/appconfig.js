51e4a16eb41169651a709c526320212cb66a60d9
Edmond Cheng <edmond.cang@gmail.com>
2018-03-01 03:50:51 +0800

    Ask google for places

---
+++ src/appconfig.js
@@ -0,0 +1,6 @@
+// Every API key has call limit
+export default {
+  // googleApiKey: 'AIzaSyD4UZ-WTw9CF0aTo9vn5qjAHr1TBMrfYNE',
+  googleApiKey: 'AIzaSyAr5MXuzv8FA_SUi6kOMfbJzftyaY8S5wk',
+  // googleApiKey: 'AIzaSyCZBk8r-zbGB3S5NHNN4XySDZ6WR26GvNI',
+};
--- src/middleWares/api.js
+++ src/middleWares/api.js
@@ -1,5 +1,8 @@
+import urlencode from 'urlencode';
 import express from 'express';
+import fetch from 'node-fetch';
 import db from '../db';
+import appconfig from '../appconfig';
 
 const router = express.Router();
 
@@ -8,6 +11,7 @@ const movies = db.collection('movies');
 
 const cache = {
   movie_names: null,
+  locations: {},
 };
 
 router.get('/movie_names', (req, res) => {
@@ -33,6 +37,52 @@ router.get('/movie_names', (req, res) => {
     })
     .then(data => res.json({ data }))
     .catch(error => res.json({ error }));
+});
+
+async function askPlace(str) {
+  const placeName = urlencode(`${str}, San Francisco`);
+  const url = [
+    'https://maps.googleapis.com/maps/api',
+    `/place/textsearch/json?query=${placeName}`,
+    `&key=${appconfig.googleApiKey}`,
+  ].join('');
+  // console.log(url);
+  try {
+    const { results } = await fetch(url).then(res => res.json());
+    return results[0];
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+router.get('/locations/:name', async (req, res) => {
+  const { name } = req.params;
+  if (cache.locations[name]) {
+    res.json({ data: cache.locations[name] });
+    return;
+  }
+
+  const num = parseInt(name, 10);
+  const list = await movies
+    .find({ $or: [{ Title: num }, { Title: name }] })
+    .project({ Locations: 1 })
+    .toArray()
+    .catch(error => res.json({ error }));
+
+  const f = async n => {
+    if (n < 0) {
+      res.json({ data: list });
+      cache.locations[name] = list;
+      return;
+    }
+    const place = await askPlace(list[n].Locations);
+    // console.log(place);
+    list[n].place = place;
+    await f(n - 1);
+  };
+
+  await f(list.length - 1);
 });
 
 router.get('/onemovie', (req, res) => {
